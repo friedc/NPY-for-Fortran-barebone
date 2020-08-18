30cebd6f1f438e0782cf3967ad03421ab87692ef
friedc <friedc@users.noreply.github.com>
2020-08-18 20:13:49 +0200

    Update example.py

--- example.py
+++ example.py
@@ -1,23 +1,23 @@
+#!/usr/bin/env python
 import numpy as np
 
-
-a_soll =  np.zeros((1000,20), dtype=np.complex64)
+a_soll =  np.zeros((10,20), dtype=np.double)
 for ind in range(a_soll.shape[0]):
     for jnd in range(a_soll.shape[1]):
         i =  ind + 1
         j =  jnd +  1
-        a_soll[ind,jnd] = - i * 0.3 + 1j*(  j*j + 0.4)
+        a_soll[ind,jnd] = i * j
 
-b_soll =  np.zeros(1200, dtype=np.complex64)
+b_soll =  np.zeros(10, dtype=np.double)
 for ind in range(b_soll.shape[0]):
     i =  ind +  1
-    b_soll[ind] = - i * 0.3 + 1j*( i + 0.4)
+    b_soll[ind] = 2 * i
 
-
-a =  np.load("mtx.npy")
-b =  np.load("vec.npy")
+a =  np.load("a.npy")
+b =  np.load("b.npy")
 
 print("A: ")
-print(np.max(np.abs(a - a_soll)/a_soll  ))
+print( np.max(np.abs(a - a_soll) / a_soll) )
+
 print("B: ")
-print(np.max(np.abs(b -   b_soll) / b_soll ))
+print( np.max(np.abs(b - b_soll) / b_soll) )
