054dc14024fb9401542af56f9d5356fd49073480
friedc <friedc@users.noreply.github.com>
2020-08-18 22:05:35 +0200

    Set standard to f2003. Formatting.

--- example.py
+++ example.py
@@ -4,17 +4,17 @@ import numpy as np
 a_soll =  np.zeros((10,20), dtype=np.double)
 for ind in range(a_soll.shape[0]):
     for jnd in range(a_soll.shape[1]):
-        i =  ind + 1
-        j =  jnd +  1
+        i = ind + 1
+        j = jnd + 1
         a_soll[ind,jnd] = i * j
 
 b_soll =  np.zeros(10, dtype=np.double)
 for ind in range(b_soll.shape[0]):
-    i =  ind +  1
+    i =  ind + 1
     b_soll[ind] = 2 * i
 
-a =  np.load("a.npy")
-b =  np.load("b.npy")
+a = np.load("a.npy")
+b = np.load("b.npy")
 
 print("A: ")
 print( np.max(np.abs(a - a_soll) / a_soll) )
--- src/example.f90
+++ src/example.f90
@@ -1,18 +1,18 @@
 program main
     use m_npy, only: save_npy
-
+    implicit none
     real(8) :: a(10,20), b(10)
     integer(8) :: i, j
 
-    do i =  1,size(a,1)
-        do j =  1,size(a,2)
+    do i = 1,size(a,1)
+        do j = 1,size(a,2)
             a(i,j) = i * j
         enddo
     enddo
     call save_npy("a.npy", a)
 
-    do i =  1,size(b,1)
-        b(i) = 2 *  i
+    do i = 1,size(b,1)
+        b(i) = 2 * i
     enddo
     call save_npy("b.npy", b)
 end program main
