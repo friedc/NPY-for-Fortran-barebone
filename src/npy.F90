4bc5907d81df2171e4889e4f0cf4211891196ef7
friedc <friedc@users.noreply.github.com>
2022-09-29 18:30:49 +0200

    Inline everything.

--- src/npy.F90
+++ src/npy.F90
@@ -14,30 +14,21 @@ contains
         character(len=*), intent(in) :: filename;\
         PTYPE, intent(in) :: PNAME PSHAPE;\
         character(len=:), allocatable :: header;\
-        integer(4) :: header_len;\
-        header = dict_str(PSTR, shape(PNAME));\
-        header_len = len(header);\
+        character(len=13):: num_str;\
+        integer :: i;\
+        header = "{'descr':'" // PSTR // "','fortran_order':True,'shape':(";\
+        do i = 1, size(shape(PNAME));\
+            write (num_str, "(I13)") size(PNAME, i);\
+            header = header // trim(adjustl(num_str));\
+            if (size(shape(PNAME)).eq.1 .or. i.lt.size(shape(PNAME))) header = header // ",";\
+        enddo;\
+        header = header // ")}"  // repeat(" ", mod(16-mod(6+1+1+4+len(header)+2+1, 16), 16)) // achar(10);\
         open(NPY_UNIT, file=filename, form="unformatted", access="stream");\
-        write (NPY_UNIT) achar(int(Z'93')) // "NUMPY", achar(2) // achar(0), header_len, header, PNAME ;\
+        write (NPY_UNIT) achar(int(Z'93')) // "NUMPY", achar(2) // achar(0), len(header, 4), header, PNAME ;\
         close(NPY_UNIT);\
     end subroutine SNAME
     GENSUB(write_int64_mtx,integer(8),int64_mtx,(:,:),"<i8")
     GENSUB(write_int64_vec,integer(8),int64_vec,(:),"<i8")
     GENSUB(write_dbl_mtx,real(8),dbl_mtx,(:,:),"<f8")
     GENSUB(write_dbl_vec,real(8),dbl_vec,(:),"<f8")
-#undef GENSUB
-    function dict_str(var_type, var_shape) result(str)
-        character(len=*), intent(in)  :: var_type
-        integer, intent(in)           :: var_shape(:)
-        character(len=:), allocatable :: str
-        character(len=13)             :: small_str
-        integer                       :: i
-        str = "{'descr':'" // var_type // "','fortran_order':True,'shape':("
-        do i = 1, size(var_shape)
-            write (small_str, "(I13)") var_shape(i)
-            str = str // trim(adjustl(small_str))
-            if (size(var_shape).eq.1 .or. i.lt.size(var_shape)) str = str // ","
-        enddo
-        str = str // ")}"  // repeat(" ", mod(16-mod(6+1+1+4+len(str)+2+1, 16), 16)) // achar(10)
-    end function dict_str
 end module m_npy
